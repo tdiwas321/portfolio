e2c1dcc43175f9790eb28eebcf3cd7eb61d15d00
Diwas Thapa <tdiwas321@gmail.com>
2024-02-19 19:00:16 +0545

    sadfsadf

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-
-import Header from './components/Header'
-import './App.css'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Work from './components/Work'
-import Skills from './components/Skills'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import "./App.css";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Work from "./components/Work";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Hero/>
-      <Projects/>
-      <Work/>
-      <Skills/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <section className="wrapper">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <Projects />
+        <Work />
+        <Skills />
+        <Footer />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
